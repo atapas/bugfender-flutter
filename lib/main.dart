790a495317a97b965e244320f777c8a860fd028f
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-05-23 12:42:54 +0530

    feat: Made a few changes like color, text in the app

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter & Bugfender Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -19,9 +19,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter & Bugfender Demo'),
     );
   }
 }
@@ -93,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'We will log the count to Bugfender once you push it',
             ),
             Text(
               '$_counter',
